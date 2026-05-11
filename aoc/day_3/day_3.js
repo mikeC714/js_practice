b5c2d37fc064569fb30e87240080c282bf60cdea
space <carter71404@icloud.com>
2026-05-10 20:03:32 -0400

    Just having fun and improving problem solving skills

--- aoc/day_3/day_3.js
+++ aoc/day_3/day_3.js
@@ -1,5 +1,5 @@
 const fs = require("node:fs");
-const INPUT = fs.readFileSync("./input.txt", "utf-8").split("\n");
+const INPUT = fs.readFileSync("./input.txt", "utf-8").split("");
 
 
 /*
@@ -16,3 +16,40 @@ For example:
     ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
 
 */
+
+
+/*
+     ^ NORTH
+     v SOUTH
+     > RIGHT
+     < LEFT
+
+     All directions can be thought of as a grid 
+     
+     NORTH y axis + 1
+     SOUTH y axis - 1
+     WEST x axis - 1 
+     EAST x axis + 1 
+*/
+
+
+let single = ["0x0"];
+let coords = {x: 0, y: 0};
+let newCoords = {x: 0, y: 0};    
+
+for(let i = 0; i < INPUT.length; i++){
+    if(INPUT[i] === "^") newCoords = {x: coords.x, y: coords.y +1};
+    if(INPUT[i] === "v") newCoords = {x: coords.x, y: coords.y -1};
+    if(INPUT[i] === ">") newCoords = {x: coords.x +1, y: coords.y};
+    if(INPUT[i] === "<") newCoords = {x: coords.x -1, y: coords.y}
+
+    const key = `${newCoords.x}x${newCoords.y}`;
+
+    if(!single.includes(key)){
+        single.push(`${newCoords.x}x${newCoords.y}`)
+    }
+    coords = newCoords  // -> GIVES THE CURRENT STATE OF COORDS 
+    console.log(coords)
+}
+
+console.log(single.length)
